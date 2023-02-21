9b5f38b764dca7fdfe5cfc5563f23c3a3dc3c2e2
devnotepage <devnotepage@gmail.com>
2023-02-21 21:39:06 +0900

    update

---
+++ cs-mqtt-arduino-aws/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_mqtt_arduino_aws
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
