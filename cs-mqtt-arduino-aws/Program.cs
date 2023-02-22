03a408c029653442a37321eb42a243a26cf695a0
devnotepage <devnotepage@gmail.com>
2023-02-22 23:04:20 +0900

    update

--- cs-mqtt-arduino-aws/Program.cs
+++ cs-mqtt-arduino-aws/Program.cs
@@ -12,9 +12,7 @@
             // シリアル通信(WindowsPC<->Arduino)
             using var serial = new SerialCommunication("COM4", 9600, OnSerialReceived);
             // MQTT通信(WindowsPC<->AWS)
-
-
-
+            using var mqtt = new MqttCommunication("COM4", 9600, OnMqttReceived);
             // コマンド入力待機
             for (; ; )
             {
@@ -25,12 +23,20 @@
             Console.WriteLine("End.");
         }
         /// <summary>
-        /// シリアル通信でデータ受信に実行されます。
+        /// シリアル通信でデータ受信時に実行されます。
         /// </summary>
         /// <param name="data"></param>
         private static void OnSerialReceived(string data)
         {
-            Console.WriteLine($"OnReceived[{data}]");
+            Console.WriteLine($"OnSerialReceived[{data}]");
+        }
+        /// <summary>
+        /// MQTT通信でデータ受信時に実行されます。
+        /// </summary>
+        /// <param name="data"></param>
+        private static void OnMqttReceived(string data)
+        {
+            Console.WriteLine($"OnMqttReceived[{data}]");
         }
     }
 }
