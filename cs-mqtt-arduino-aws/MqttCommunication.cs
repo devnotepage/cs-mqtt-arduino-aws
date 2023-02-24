c42af02e42cbf4f1d22384cb12c829491744d2a9
devnotepage <devnotepage@gmail.com>
2023-02-24 21:35:34 +0900

    update

--- cs-mqtt-arduino-aws/MqttCommunication.cs
+++ cs-mqtt-arduino-aws/MqttCommunication.cs
@@ -1,31 +1,61 @@
-﻿namespace cs_mqtt_arduino_aws
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Packets;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace cs_mqtt_arduino_aws
 {
     /// <summary>
     /// MQTT通信
     /// </summary>
     internal class MqttCommunication : IDisposable
     {
+        private MqttFactory _mqttFactory;
+        private IMqttClient _mqttClient;
+        private MqttClientOptions _mqttClientOptions;
+        private MqttClientSubscribeOptions _mqttSubscribeOptions;
+        private string _topic;
         private Action<string> _onReceived;
-        internal MqttCommunication(string port, int speed, Action<string> onReceived)
+        internal MqttCommunication(string server, string topic, Action<string> onReceived)
         {
+            _topic = topic;
             _onReceived = onReceived;
-            Task.Run(() =>
+            _mqttFactory = new MqttFactory();
+            _mqttClient = _mqttFactory.CreateMqttClient();
+            _mqttClient.ApplicationMessageReceivedAsync += e =>
             {
-                for (; ; )
-                {
-                    _onReceived(Receive());
-                }
-            });
+                Console.WriteLine("Received application message.");
+                Console.WriteLine(e.ToString());
+                _onReceived(e.ToString());
+                return Task.CompletedTask;
+            };
+            _mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(server)
+                .Build();
+            _mqttClient.ConnectAsync(_mqttClientOptions, CancellationToken.None);
+            _mqttSubscribeOptions = _mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(
+                    f =>
+                    {
+                        f.WithTopic(_topic);
+                    })
+                .Build();
+            _mqttClient.SubscribeAsync(_mqttSubscribeOptions, CancellationToken.None);
         }
-        internal void Send(string data)
+        public void Send(string data)
         {
-        }
-        private string Receive()
-        {
-            return string.Empty;
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic(_topic)
+                .WithPayload(data)
+                .Build();
+            _mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+            Console.WriteLine("MQTT application message is published.");
         }
         public void Dispose()
         {
+            _mqttClient.DisconnectAsync();
+            _mqttClient.Dispose();
         }
     }
 }
--- cs-mqtt-arduino-aws/Program.cs
+++ cs-mqtt-arduino-aws/Program.cs
@@ -12,7 +12,8 @@
             // シリアル通信(WindowsPC<->Arduino)
             using var serial = new SerialCommunication("COM4", 9600, OnSerialReceived);
             // MQTT通信(WindowsPC<->AWS)
-            using var mqtt = new MqttCommunication("COM4", 9600, OnMqttReceived);
+            using var mqtt = new MqttCommunication("broker.hivemq.com", "topic", OnMqttReceived);
+            mqtt.Send("Start.");
             // コマンド入力待機
             for (; ; )
             {
@@ -20,12 +21,13 @@
                 if (cmd == "exit") { break; }
                 serial.Send(cmd);
             }
+            mqtt.Send("End.");
             Console.WriteLine("End.");
         }
         /// <summary>
         /// シリアル通信でデータ受信時に実行されます。
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="data">受信データ</param>
         private static void OnSerialReceived(string data)
         {
             Console.WriteLine($"OnSerialReceived[{data}]");
@@ -33,7 +35,7 @@
         /// <summary>
         /// MQTT通信でデータ受信時に実行されます。
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="data">受信データ</param>
         private static void OnMqttReceived(string data)
         {
             Console.WriteLine($"OnMqttReceived[{data}]");
